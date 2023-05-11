1996f9d8d59a38616badbce373d768b9a6cbf7ba
Gilberto Mascena <gilbertomascena@gmail.com>
2023-05-11 20:08:56 -0300

    GET game by id

--- dslist/src/main/java/com/gilberto/dslist/services/GameService.java
+++ dslist/src/main/java/com/gilberto/dslist/services/GameService.java
@@ -4,19 +4,28 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.gilberto.dslist.dto.GameDTO;
 import com.gilberto.dslist.dto.GameMinDTO;
 import com.gilberto.dslist.entities.Game;
 import com.gilberto.dslist.repositories.GameRepository;
 
 @Service
 public class GameService {
-    
+
     @Autowired
     private GameRepository gameRepository;
 
-    public List<GameMinDTO> findAll(){    
+    @Transactional(readOnly = true)
+    public GameDTO findById(Long id) {
+        Game result = gameRepository.findById(id).get();
+        return new GameDTO(result);
+    }
+
+    @Transactional(readOnly = true)
+    public List<GameMinDTO> findAll() {
         List<Game> result = gameRepository.findAll();
-        return result.stream().map(x -> new GameMinDTO(x)).toList();        
+        return result.stream().map(x -> new GameMinDTO(x)).toList();
     }
 }
