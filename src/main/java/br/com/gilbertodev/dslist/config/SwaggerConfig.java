510af6172905becd34606712324f466fd06abd29
Gilberto Mascena <gilbertomascena@gmail.com>
2024-12-01 18:57:35 -0300

    :sparkles: feat: added license in Swagger class

--- src/main/java/br/com/gilbertodev/dslist/config/SwaggerConfig.java
+++ src/main/java/br/com/gilbertodev/dslist/config/SwaggerConfig.java
@@ -2,6 +2,7 @@ package br.com.gilbertodev.dslist.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,6 +14,9 @@ public class SwaggerConfig {
         return new OpenAPI().info(new Info()
                 .title("DSList API")
                 .version("1.0.0")
-                .description("Documentation of DSList API"));
+                .description("Documentation of DSList API")
+                .license(new License()
+                        .url("https://github.com/Gilberto-Mascena/dslist/blob/main/LICENSE.md")
+                        .name("MIT License")));
     }
 }
