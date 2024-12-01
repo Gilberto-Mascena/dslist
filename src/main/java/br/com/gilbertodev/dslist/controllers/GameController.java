f199d10c969877a94abe7b38fb6a1248dc1838ed
Gilberto Mascena <gilbertomascena@gmail.com>
2024-12-01 18:25:24 -0300

    :sparkles: feat: added description to routes with Swagger

--- src/main/java/br/com/gilbertodev/dslist/controllers/GameController.java
+++ src/main/java/br/com/gilbertodev/dslist/controllers/GameController.java
@@ -3,6 +3,7 @@ package br.com.gilbertodev.dslist.controllers;
 import br.com.gilbertodev.dslist.dto.GameDTO;
 import br.com.gilbertodev.dslist.dto.GameMinDTO;
 import br.com.gilbertodev.dslist.services.GameService;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.annotation.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,12 +19,14 @@ public class GameController {
     @Resource
     private GameService gameService;
 
+    @Operation(summary = "Find a game by id", description = "Find a game by id and return the game", tags = {"game by id"})
     @GetMapping(value = "/{id}")
     public GameDTO findById(@PathVariable Long id) {
         GameDTO result = gameService.findById(id);
         return result;
     }
 
+    @Operation(summary = "Find all games", description = "Find all games and return a list of games", tags = {"all games"})
     @GetMapping
     public List<GameMinDTO> findAll() {
         List<GameMinDTO> result = gameService.findAll();
--- src/main/java/br/com/gilbertodev/dslist/controllers/GameListController.java
+++ src/main/java/br/com/gilbertodev/dslist/controllers/GameListController.java
@@ -5,6 +5,7 @@ import br.com.gilbertodev.dslist.dto.GameMinDTO;
 import br.com.gilbertodev.dslist.dto.ReplacementDTO;
 import br.com.gilbertodev.dslist.services.GameListService;
 import br.com.gilbertodev.dslist.services.GameService;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.annotation.Resource;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,18 +21,21 @@ public class GameListController {
     @Resource
     private GameService gameService;
 
+    @Operation(summary = "Find all lists", description = "Find all lists and their games", tags = {"lists catalog"})
     @GetMapping
     public List<GameListDTO> findAll() {
         List<GameListDTO> result = gameListService.findAll();
         return result;
     }
 
+    @Operation(summary = "Find list by id", description = "Find a list by its id and its game", tags = {"lists by id"})
     @GetMapping(value = "/{listId}/games")
     public List<GameMinDTO> findByList(@PathVariable Long listId) {
         List<GameMinDTO> result = gameService.findByList(listId);
         return result;
     }
 
+    @Operation(summary = "Move game", description = "Move a game's position in the list", tags = {"move game"})
     @PostMapping(value = "/{listId}/replacement")
     public void move(@PathVariable Long listId, @RequestBody ReplacementDTO body) {
         gameListService.move(listId, body.getSourceIndex(), body.getDestinationIndex());
