93f8bd83868f930b43c34e54c6a8c5b67eadd8f6
Gilberto Mascena <gilbertomascena@gmail.com>
2025-06-13 20:02:15 -0300

    :books: Updated Javadoc exceptions

--- src/main/java/br/com/gilbertodev/dslist/exceptions/GameNotFoundException.java
+++ src/main/java/br/com/gilbertodev/dslist/exceptions/GameNotFoundException.java
@@ -4,6 +4,7 @@ package br.com.gilbertodev.dslist.exceptions;
  * Exception thrown when a game with the specified ID is not found.
  *
  * @author Gilberto Dev
+ * @since 1.0.0
  */
 public class GameNotFoundException extends RuntimeException {
 
--- src/main/java/br/com/gilbertodev/dslist/exceptions/InvalidMoveException.java
+++ src/main/java/br/com/gilbertodev/dslist/exceptions/InvalidMoveException.java
@@ -4,6 +4,7 @@ package br.com.gilbertodev.dslist.exceptions;
  * Exception thrown when a move operation is invalid.
  *
  * @author Gilberto Dev
+ * @since 1.0.0
  */
 public class InvalidMoveException extends RuntimeException {
 
