d528ef68110a02502e213b0677b6eef6a677d89e
Gilberto Mascena <gilbertomascena@gmail.com>
2025-08-03 12:47:39 -0300

    :recycle: refactor: Updating imports in the Game class

--- src/main/java/br/com/gilbertodev/dslist/entities/Game.java
+++ src/main/java/br/com/gilbertodev/dslist/entities/Game.java
@@ -1,6 +1,11 @@
 package br.com.gilbertodev.dslist.entities;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 /**
  * Represents a game entity that is stored in the "tb_game" database table.
